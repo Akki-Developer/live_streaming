cba132b4ef51d15f8b6125de1242a947494f89f5
Akanksha Gupta <64725938+Akki-Developer@users.noreply.github.com>
2021-08-23 15:05:58 +0530

    update websocket url as wss

--- client.js
+++ client.js
@@ -3,7 +3,7 @@ var name;
 var connectedUser;
   
 //connecting to our signaling server
-var conn = new WebSocket('ws://mobikul-saas.webkul.com:9090'); 
+var conn = new WebSocket('wss://mobikul-saas.webkul.com:9090'); 
   
 conn.onopen = function () { 
    console.log("Connected to the signaling server"); 
